27cd78999fe8b8cb99c4af43d5481892adbdebe5
Kyle Lahnakoski <klahnakoski@mozilla.com>
2017-02-09 17:19:34 -0500

    fix script

--- resources/scripts/treeherder_extract.sh
+++ resources/scripts/treeherder_extract.sh
@@ -4,5 +4,5 @@
 
 cd ~/MySQL-to-S3
 export PYTHONPATH=.
-python mysql_to_s3/extract.py --settings=resources/config/treeherder.json  >& /dev/null < /dev/null 
+python mysql_to_s3/extract.py --settings=resources/config/treeherder.json  >& /dev/null < /dev/null
 
